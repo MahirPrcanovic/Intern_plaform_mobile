f589a3307283bb497ba6293ce3f5ffd1d20df107
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-12-30 15:48:38 +0100

    initial commit

---
+++ InternshipPlatform-Mobile/InternshipPlatform-Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InternshipPlatform_Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ InternshipPlatform-Mobile/InternshipPlatform-Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace InternshipPlatform_Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
