b614e84bba0af9786c2f6baa7802d615452ac09d
MahirPrcanovic <mahirprcanovic7@gmail.com>
2023-01-08 22:22:27 +0100

    feat: login page created

--- InternshipPlatform-Mobile/InternshipPlatform-Mobile/MainPage.xaml.cs
+++ InternshipPlatform-Mobile/InternshipPlatform-Mobile/MainPage.xaml.cs
@@ -2,23 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await DisplayAlert("Title","The email is "+email.Text,"OK");
+    }
 }
 
