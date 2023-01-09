63d66c4fb81f715a18d56041cccc35dda0c46e86
MahirPrcanovic <mahirprcanovic7@gmail.com>
2023-01-09 18:01:14 +0100

    feat: applications page inserted

---
+++ SQLiteMAUI/People/Models/Ss.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.Models
+{
+    public class Ss
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string EducationLevel { get; set; }
+
+        public string CoverLetter { get; set; }
+        public string CV { get; set; }
+    }
+}
--- SQLiteMAUI/People/Pages/ApplicantsPage.xaml.cs
+++ SQLiteMAUI/People/Pages/ApplicantsPage.xaml.cs
@@ -1,12 +1,42 @@
+using People.Models;
 namespace People.Pages;
 
 public partial class ApplicantsPage : ContentPage
 {
-	string UserName;
-	public ApplicantsPage()
+    public IList<Ss> StudentList { get; set; }
+    public ApplicantsPage()
 	{
 		InitializeComponent();
-		UserName = "Mahir";
+		userName.Text = "Mahir P.";
+        StudentList = new List<Ss> {
+           new Ss
+           {
+               
+               FirstName="Mahir",
+               LastName="Prcanovic",
+               Email="mahirprcanovic@gmail.com",
+               EducationLevel = "nesto",
+               CV = "nesto",
+               CoverLetter="Nesto"
+           },
+           new Ss
+           {
+               
+               FirstName="Mahir2",
+               LastName="Prcanovic2",
+               Email="mahirprcanovic2@gmail.com",
+               EducationLevel = "nesto",
+               CV = "nesto",
+               CoverLetter="Nesto"
+           }
+        };
 		BindingContext= this;
 	}
+
+    private void ListView_ItemTapped_1(object sender, ItemTappedEventArgs e)
+    {
+        //var myListView = (ListView)sender;
+        //var odabranaTura = (Student)myListView.SelectedItem;
+        //await Navigation.PushModalAsync(new NavigationPage(new DetaljiTure(registrovanikorisnik, odabranaTura)));
+    }
 }
